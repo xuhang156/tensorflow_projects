84b56583989923e2833507f8e811a11f7b5dd07e
徐航 | Hang Xu <xu.hang@mech-mind.net>
2024-07-30 23:00:39 +0800

    feat：增加对图像归一化函数

--- mnist/train.py
+++ mnist/train.py
@@ -9,6 +9,14 @@ train_labels_path = './MNIST/train-labels-idx1-ubyte.gz'
 test_images_path = './MNIST/t10k-images-idx3-ubyte.gz'  
 test_labels_path = './MNIST/t10k-labels-idx1-ubyte.gz'  
 
+def binaryzation_images(images):
+    shape = images.shape
+    width = shape[1]
+    height = shape[2]
+    images = images.reshape((shape[0],width * height))
+    images = images.astype('float32') /255
+    return images
+
 def load_mnist_images(file_path):  
     with gzip.open(file_path, 'rb') as f:  
         # Skip the magic number and dimensions  
@@ -42,3 +50,7 @@ print(f"Training labels shape: {train_labels.shape}")
 print(f"Training labels (example): {train_labels[:5]}")  # 只打印前5个  
 print(f"Test data shape: {test_images.shape}")  
 print(f"Test labels shape: {test_labels.shape}")
+
+train_images = binaryzation_images(train_images)
+print(f"Training data shape: {train_images.shape}")  
+print(f"Training labels shape: {train_labels.shape}")  
